1fda982c7735f79d285488b855770f9f26883a61
Jasper Palfree <well.caffeinated@gmail.com>
2012-10-25 17:02:43 -0400

    added analytics for outbound links

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -397,6 +397,27 @@ define(
                 ,bounds
                 ;
 
+            $(document).on('click', 'a[href^=http]', function(e){
+
+                var $this = $(this)
+                    ,url = $this.attr('href')
+                    ,newtab = ($this.attr('target') === '_blank' || e.metaKey || e.ctrlKey)
+                    ;
+
+                window._gaq = window._gaq || [];
+                
+                try {
+
+                    window._gaq.push(['_trackEvent', 'Outbound Links', e.currentTarget.host, url, 0]);
+                    
+                    if (!newtab) {
+
+                        e.preventDefault();
+                        setTimeout('document.location = "' + url + '"', 100);
+                    }
+                } catch (err){}
+            });
+
             //$(window).on('resize', resize);
 
             $(function(){
